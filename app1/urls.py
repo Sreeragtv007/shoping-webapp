f66ff3f2943e242127d16cb71df648c2d355e6d9
sreerag <sreeragtv007@gmail.com>
2023-09-29 12:06:33 +0530

    error

--- app1/urls.py
+++ app1/urls.py
@@ -4,8 +4,6 @@ from app1 import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('search/',views.search_product,name='search'),
-    path('register/',views.register,name='register'),
-    path('login/',views.login,name='login')
+    path('search/',views.search_product,name='search')
    
 ]
