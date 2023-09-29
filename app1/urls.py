ba977c4133b4407875c81d6a46f59a7fb8421c95
sreerag <sreeragtv007@gmail.com>
2023-09-29 12:04:36 +0530

    error

--- app1/urls.py
+++ app1/urls.py
@@ -4,6 +4,8 @@ from app1 import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('search/',views.search_product,name='search')
+    path('search/',views.search_product,name='search'),
+    path('register/',views.register,name='register'),
+    path('login/',views.login,name='login')
    
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import product,category
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -16,9 +19,42 @@ def home(request):
 
 def search_product(request):
     search=request.GET.get('searching')
-    obj=product.objects.filter(name__icontains=search )
-    context={'obj':obj}
-    return render(request,'product.html',context)
+    
+    obj=product.objects.filter(name__icontains=search)
+    if obj is not None:
+        context={'obj':obj}
+        return redirect('index')
+
+    else:
+
+        return render(request,'index.html',context)
+    
+def register(request):
+    if request.POST:
+        uname=request.POST.get('username')
+        
+        pass1=request.POST.get('pass1')
+        pass2=request.POST.get('pass2')
+        if User.objects.filter(username=uname).exists():
+            messages.info(request,"username taken")
+            return redirect('register')
+        elif pass1==pass2:
+            user=User.objects.create_user(username=uname,password=pass1)
+            user.save()
+            return redirect('login')
+        else:
+            messages.info(request,"password does not match")
+            return redirect('register')            
+        
+            
+
+            
+    
+    return render(request,'register.html')
+
+def login(request):
+     
+    return render(request,'login.html')
 
 
 
