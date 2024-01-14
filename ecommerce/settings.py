d956abbd3bd342914597e4ed335f9990dfbba595
Sreeragtv007 <97658330+Sreeragtv007@users.noreply.github.com>
2024-01-14 11:45:38 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -159,6 +159,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'digitalmediaupdates007@gmail.com'
-EMAIL_HOST_PASSWORD = 'uwmjcquwrtjdvgrd'
+EMAIL_HOST_PASSWORD = ''
 
 
